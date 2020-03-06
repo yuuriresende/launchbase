c0504e9ac41c0ef67da5f7782ef7a9cc6496e40e
Yuri Resende <yuuriresende@hotmail.com>
2020-03-06 15:18:58 -0300

    sintaxe do javascript

--- fase02/coding/index.js
+++
@@ -1,3 +0,0 @@
-//Criar um programa que calcula a média
-// das notas entre alunos e envia
-// mensagem do calculo da média.
---
+++ fase02/coding/sintaxe.js
@@ -0,0 +1,20 @@
+//Criar um programa que calcula a média
+// das notas entre alunos e envia
+// mensagem do calculo da média.
+
+const nome = "Yuri"
+const nome2 = 'José'
+const nome3 = `Valeria ${nome}` //template string, aceita variavel dentro.
+
+
+const aluno01 = 'Joao'
+const aluno02 = 'Maria'
+const aluno03 = 'José'
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 8.5
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
+
+console.log(media)
+
