ba9e608f6925e023749e71eac325341596ec902f
Yuri Resende <yuuriresende@hotmail.com>
2020-04-20 14:29:46 -0300

    inicio da retirada de elementos do front, para o beckend

---
+++ fase03/data.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    id : "GykTLqODQuU",
+    title: "Formulário animado com JS puro e CSS Animation",
+    duration: "57 min",
+    price: "Free",
+    imageFile: "images/card01.jpg"
+   
+  },
+
+  {
+    id : "HN1UjzRSdBk",
+    title: "Desvendando o css grid na prática",
+    duration: "36 min",
+    price: "Free",
+    imageFile: "images/card02.jpg"
+  },
+
+  {
+    id : "u99tNt3TZf8",
+    title: "Desvendando Redux na prática",
+    duration: "42 min",
+    price: "Free",
+    imageFile: "images/card03.jpg"
+  },
+
+  {
+    id : "ghTrp1x_1As",
+    title: "O que é api? Rest e Restful?",
+    duration: "51 min",
+    price: "Free",
+    imageFile: "images/card04.jpg"
+  },
+
+  {
+    id : "2alg7MQ6_sI",
+    title: "GIT e GitHub na prática",
+    duration: "32 min",
+    price: "Free",
+    imageFile: "images/card05.jpg"
+  },
+
+  {
+    id : "R3S8DEzEn6s",
+    title: "Styled Components",
+    duration: "30 min",
+    price: "Free",
+    imageFile: "images/card06.jpg"
+  }
+]
--- fase03/server.js
+++ fase03/server.js
@@ -2,9 +2,11 @@ const express = require ('express')
 const nunjucks = require ('nunjucks')
 
 const server = express()
+const videos = require ("./data")
+
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 nunjucks.configure("views", {
   express: server
 })
@@ -14,7 +16,8 @@ server.get("/", function(req, res){
 })
 
 server.get("/projetos", function(req, res){
-  return res.render("projetos")
+
+  return res.render("projetos", { items: videos })
 })
 
 server.listen(5000, function(){
